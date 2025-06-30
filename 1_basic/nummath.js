1c2ac204d7ce9938c7d0805c4150f4cb5c15f538
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-06-30 03:59:34 +0000

    Math And Numbers

---
+++ 1_basic/nummath.js
@@ -0,0 +1,16 @@
+let num = 4.7;
+
+// console.log(Math.round(num));   // 5
+// console.log(Math.floor(num));   // 4
+// console.log(Math.ceil(num));    // 5
+// console.log(Math.trunc(num));   // 4
+
+// console.log(Math.sqrt(49));     // 7
+// console.log(Math.pow(2, 4));    // 16
+// console.log(Math.abs(-100));    // 100
+
+// console.log(Math.max(10, 2, 100)); // 100
+// console.log(Math.min(10, 2, 100)); // 2
+
+// Random number between 1 and 6 (like a dice)
+console.log(Math.floor(Math.random() * 6) + 1);
