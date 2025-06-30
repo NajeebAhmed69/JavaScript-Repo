73b0f56d01311919a0c08f2469c03dcd8a785412
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-06-30 15:07:08 +0000

    this and arrow function

---
+++ 1_basic/thisarrow.js
@@ -0,0 +1,16 @@
+// const person = {
+//   name: "Najeeb",
+//   greet() {
+//     console.log(`Hi, my name is ${this.name}`); // `this` refers to `person`
+//   }
+// };
+
+// person.greet(); // "Hi, I'm Najeeb"
+
+// Regular function
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // Arrow function (short form)
+// const add = (a, b) => a + b;
