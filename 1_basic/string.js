c4b66bead53b9a73d1d85759cf1f95416d96c8e1
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-06-30 03:32:41 +0000

    string and its operation

---
+++ 1_basic/string.js
@@ -0,0 +1,28 @@
+// let name1 = "Najeeb";       // Double quotes
+// let name2 = ' Ahmed';        // Single quotes
+// let name3 = ` Hello`;        // Backticks (Template Literal)
+
+
+// console.log(name1+name2+name3+" hello")
+
+
+// console.log(`${name3} my name is ${name1+name2}`)
+
+console.log("hello".length)
+console.log("js".toUpperCase())
+console.log("JS".toLowerCase())
+console.log(" hi ".trim())
+console.log("JavaScript".slice(0, 4))
+console.log("abc".substring(1, 3))
+console.log("abcde".substr(1, 3))
+console.log(	"abc".charAt(1))
+console.log(	"abcabc".indexOf("b"))
+console.log("abcabc".lastIndexOf("b"))
+console.log("hello".includes("ll"))
+console.log("abc".startsWith("a"))
+console.log("abc".endsWith("c"))
+console.log("js is good".replace("good", "great"))
+console.log(	"ha".repeat(3))
+console.log("a,b,c".split(","))
+console.log("Hello".concat(" World"))
+
