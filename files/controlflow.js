e8b38b55d3634da7f7ee4b739b59963b608c80c2
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-07-01 10:29:48 +0000

    control flow

--- files/controlflow.js
+++ files/controlflow.js
@@ -1,40 +1,54 @@
-let age = 18;
+// let age = 18;
 
-if (age >= 18) {
-  console.log("You are an adult");
-}
-//Executes only if the condition is true.
+// if (age >= 18) {
+//   console.log("You are an adult");
+// }
+// //Executes only if the condition is true.
 
-let isLoggedIn = false;
+// let isLoggedIn = false;
 
-if (isLoggedIn) {
-  console.log("Welcome back!");
-} else {
-  console.log("Please log in.");
-}
+// if (isLoggedIn) {
+//   console.log("Welcome back!");
+// } else {
+//   console.log("Please log in.");
+// }
 
-let score = 85;
+// let score = 85;
 
-if (score >= 90) {
-  console.log("Grade A");
-} else if (score >= 75) {
-  console.log("Grade B");
-} else {
-  console.log("Grade C");
-}
+// if (score >= 90) {
+//   console.log("Grade A");
+// } else if (score >= 75) {
+//   console.log("Grade B");
+// } else {
+//   console.log("Grade C");
+// }
 
 
-let day = "Tuesday";
+// let day = "Tuesday";
 
-switch (day) {
-  case "Monday":
-    console.log("Start of week");
-    break;
-  case "Tuesday":
-    console.log("Second day");
-    break;
-  default:
-    console.log("Another day");
-}
-//switch is great for checking one value against many cases.
+// switch (day) {
+//   case "Monday":
+//     console.log("Start of week");
+//     break;
+//   case "Tuesday":
+//     console.log("Second day");
+//     break;
+//   default:
+//     console.log("Another day");
+// }
+// //switch is great for checking one value against many cases.
+
+// Nullish Coalescing Operator (??): null undefined
+// let value;
+// value=5??10
+// value= null??10
+// value= undefined??15
+
+// console.log(value)
+
+
+//Terniary Operator
+const teaPrice=100
+teaPrice >=80? console.log("less than 80"):console.log("more than 80")
+
 
