0bbfae7bb7fcd317971215b5bdca3af39ef03afc
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-07-01 10:52:59 +0000

    for loop

---
+++ files/loop1.js
@@ -0,0 +1,11 @@
+// for loop
+ for (let i = 0; i < 5; i++) {
+  console.log(i); // 0 to 4
+}
+for (let i = 1; i <=10; i++) {
+    console.log(`Table of ${i}`)
+   for (let j  = 1; j<= 10; j ++) {
+    console.log(`${i} * ${j} = ${i*j} `)
+   }
+    
+}
