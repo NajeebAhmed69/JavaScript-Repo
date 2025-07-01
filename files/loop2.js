2bc5570201f471197d97dddbb27861b385f970ff
Najeeb Ahmed <160821040+NajeebAhmed69@users.noreply.github.com>
2025-07-01 11:00:47 +0000

    while and do-while

---
+++ files/loop2.js
@@ -0,0 +1,23 @@
+//while
+// let i = 0;
+// while (i < 3) {
+//   console.log(i);
+//   i++;
+// }
+
+
+// let array=["najeeb","ahmed","ghouri"]
+// let arr=0
+// while (arr<array.length) {
+//     console.log(array[arr])
+//     arr++
+// }
+
+
+//do-while
+let score=1
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
+// do...while runs at least once, even if the condition is false.
